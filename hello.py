44f9a97f5f3c239da62477d714109e0704129287
lr131 <lr131k@gmail.com>
2015-12-09 02:20:01 +0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # показать профиль данного пользователя
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # вывести сообщение с данным id, id - целое число
+    return 'Post %d' % post_id
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
